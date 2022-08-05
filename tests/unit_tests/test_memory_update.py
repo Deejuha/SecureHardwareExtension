a60ba5c73741af1099162e4beb51ab0a3803d85e
Deejuha <Deejuha@gmail.com>
2022-08-06 01:26:28 +0200

    Reworked test docstring

--- tests/unit_tests/test_memory_update.py
+++ tests/unit_tests/test_memory_update.py
@@ -1,14 +1,15 @@
+"""
+Test vectors found in 
+https://www.autosar.org/fileadmin/user_upload/standards/foundation/19-11/AUTOSAR_TR_SecureHardwareExtensions.pdf
+
+"""
+
 from datatypes import MemoryUpdateInfo, SecurityFlags, she_bytes
 from memory_update import MemoryUpdateProtocol
 from unittest.mock import Mock
 
 
 def test_aes_compress():
-    """
-    https://www.autosar.org/fileadmin/user_upload/standards/foundation/19-11/AUTOSAR_TR_SecureHardwareExtensions.pdf
-    4.13.2.4 Miyaguchi-Preneel compression function
-
-    """
     update_info = Mock()
     expected = she_bytes.fromhex("c7277a0dc1fb853b5f4d9cbd26be40c6")
     update_protocol = MemoryUpdateProtocol(update_info)
@@ -35,9 +36,13 @@ def test_update_protocol():
     expected_k3 = she_bytes.fromhex("ed2de7864a47f6bac319a9dc496a788f")
     expected_k4 = she_bytes.fromhex("ec9386fefaa1c598246144343de5f26a")
     expected_m1 = she_bytes.fromhex("00000000000000000000000000000141")
-    expected_m2 = she_bytes.fromhex("2b111e2d93f486566bcbba1d7f7a9797c94643b050fc5d4d7de14cff682203c3")
+    expected_m2 = she_bytes.fromhex(
+        "2b111e2d93f486566bcbba1d7f7a9797c94643b050fc5d4d7de14cff682203c3"
+    )
     expected_m3 = she_bytes.fromhex("b9d745e5ace7d41860bc63c2b9f5bb46")
-    expected_m4 = she_bytes.fromhex("00000000000000000000000000000141b472e8d8727d70d57295e74849a27917")
+    expected_m4 = she_bytes.fromhex(
+        "00000000000000000000000000000141b472e8d8727d70d57295e74849a27917"
+    )
     expected_m5 = she_bytes.fromhex("820d8d95dc11b4668878160cb2a4e23e")
     update_protocol = MemoryUpdateProtocol(update_info)
     assert expected_k1 == update_protocol.k1
@@ -49,4 +54,3 @@ def test_update_protocol():
     assert expected_m3 == update_protocol.m3
     assert expected_m4 == update_protocol.m4
     assert expected_m5 == update_protocol.m5
-    
