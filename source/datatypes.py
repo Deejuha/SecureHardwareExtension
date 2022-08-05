bd7a7cc875164571ffb2bce969a5beaf59151629
Deejuha <Deejuha@gmail.com>
2022-08-05 07:37:38 +0200

    Added FID

--- source/datatypes.py
+++ source/datatypes.py
@@ -3,7 +3,7 @@ Module contains types using within the project.
 
 """
 
-__all__ = ["she_bytes"]
+__all__ = ["SecurityFlag", "she_bytes"]
 
 
 class she_bytes(bytes):
@@ -27,3 +27,45 @@ class she_bytes(bytes):
             Result of XOR.
         """
         return bytes(x ^ y for x, y in zip(self, other))
+
+
+class SecurityFlag(object):
+    def __set_name__(self, owner, name):
+        self._attribute_name = name
+
+    def __init__(self, bit_index: int):
+        self._bit_index = bit_index
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, f"_{self._attribute_name}")
+
+    def __set__(self, obj, value):
+        if not isinstance(value, bool):
+            raise TypeError("Security flag shall be type of bool.")
+        if value:
+            obj._fid = obj._fid | (1 << self._bit_index)
+        else:
+            obj._fid = obj._fid & ~(1 << self._bit_index)
+        setattr(obj, f"_{self._attribute_name}", value)
+
+
+class SecurityFlags:
+    write_protection = SecurityFlag(0)
+    boot_failure = SecurityFlag(1)
+    debugger_activation = SecurityFlag(2)
+    wildcard_usage = SecurityFlag(3)
+    key_usage = SecurityFlag(4)
+    plain_key = SecurityFlag(5)
+
+    def __init__(self):
+        self._fid: int = 0
+        self.write_protection = False
+        self.boot_failure = False
+        self.debugger_activation = False
+        self.wildcard_usage = False
+        self.key_usage = False
+        self.plain_key = False
+
+    @property
+    def fid(self):
+        return self._fid
--- tests/unit_tests/test_datatypes.py
+++ tests/unit_tests/test_datatypes.py
@@ -1,17 +1,22 @@
-from datatypes import she_bytes
-from pytest import mark
+from datatypes import SecurityFlags, she_bytes
+from pytest import fixture, mark, raises
+
+
+@fixture
+def security_flags():
+    yield SecurityFlags()
 
 
 @mark.parametrize(
     "a, b, expected",
-    [
+    (
         ("FF", "FF", "00"),
         ("00", "00", "00"),
         ("FF", "00", "FF"),
         ("FFFFFF", "FFFFFF", "000000"),
         ("FF00FF", "00FF00", "FFFFFF"),
-        ("AA55AA55AA55AA55", "0000000000000000", "AA55AA55AA55AA55")
-    ],
+        ("AA55AA55AA55AA55", "0000000000000000", "AA55AA55AA55AA55"),
+    ),
 )
 def test_bytes_xor(a, b, expected):
     bytes_a = she_bytes.fromhex(a)
@@ -19,3 +24,78 @@ def test_bytes_xor(a, b, expected):
     bytes_expected = she_bytes.fromhex(expected)
     result = bytes_a ^ bytes_b
     assert bytes_expected == result
+
+
+@mark.parametrize(
+    "flags_to_set, expected_fid",
+    (
+        ((), 0),
+        (("write_protection",), 0b000001),
+        (("boot_failure",), 0b000010),
+        (("debugger_activation",), 0b000100),
+        (("wildcard_usage",), 0b001000),
+        (("key_usage",), 0b010000),
+        (("plain_key",), 0b100000),
+        (
+            (
+                "write_protection",
+                "boot_failure",
+                "debugger_activation",
+                "wildcard_usage",
+                "key_usage",
+                "plain_key",
+            ),
+            0b111111,
+        ),
+    ),
+)
+def test_security_flags_set_fid(security_flags, flags_to_set, expected_fid):
+    for flagname in flags_to_set:
+        setattr(security_flags, flagname, True)
+    assert expected_fid == security_flags.fid
+
+
+@mark.parametrize(
+    "flags_to_clear, expected_fid",
+    (
+        ((), 0b111111),
+        (("write_protection",), 0b111110),
+        (("boot_failure",), 0b111101),
+        (("debugger_activation",), 0b111011),
+        (("wildcard_usage",), 0b110111),
+        (("key_usage",), 0b101111),
+        (("plain_key",), 0b011111),
+        (
+            (
+                "write_protection",
+                "boot_failure",
+                "debugger_activation",
+                "wildcard_usage",
+                "key_usage",
+                "plain_key",
+            ),
+            0b000000,
+        ),
+    ),
+)
+def test_security_flags_clear_fid(security_flags, flags_to_clear, expected_fid):
+    for attribute in (
+        "write_protection",
+        "boot_failure",
+        "debugger_activation",
+        "wildcard_usage",
+        "key_usage",
+        "plain_key",
+    ):
+        setattr(security_flags, attribute, True)
+    for flagname in flags_to_clear:
+        setattr(security_flags, flagname, False)
+    assert expected_fid == security_flags.fid
+
+
+@mark.parametrize(
+    "value", (1, "string")
+)
+def test_security_flags_typeerror(security_flags, value):
+    with raises(TypeError):
+        security_flags.write_protection = value
